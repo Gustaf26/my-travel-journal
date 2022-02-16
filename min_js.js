45fe428842122ea00726076ba32d10ac57bb186e
Gustaf26 <gustavo.catalasverdrup@gmail.com>
2022-02-16 20:40:34 +0100

    initial commit with list of images

---
+++ min_js.js
@@ -0,0 +1,15 @@
+let myList = document.getElementById('list')
+let addBtn = document.getElementById('add-btn')
+let newToDo = document.getElementById('todo-input')
+
+let myTravels = ['https://images.unsplash.com/photo-1549144511-f099e773c147?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1522878129833-838a904a0e9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1519451241324-20b4ea2c4220?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+]
+
+function renderTravels() {
+
+
+}
+
+renderTravels()
