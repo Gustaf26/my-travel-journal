246bb1b1c52227a38c947e785f026c383a08c039
Gustaf26 <gustavo.catalasverdrup@gmail.com>
2022-02-16 20:47:37 +0100

    changing names to match travel names

--- min_js.js
+++ min_js.js
@@ -1,6 +1,6 @@
 let myList = document.getElementById('list')
 let addBtn = document.getElementById('add-btn')
-let newToDo = document.getElementById('todo-input')
+let newToDo = document.getElementById('travel-input')
 
 let myTravels = ['https://images.unsplash.com/photo-1549144511-f099e773c147?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
     'https://images.unsplash.com/photo-1522878129833-838a904a0e9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
